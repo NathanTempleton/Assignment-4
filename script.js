b54691e058c3933658c970048865174eac0aff76
NathanTempleton <nathan.templeton@ocsbstudent.ca>
2021-03-22 17:07:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,100 @@
+document.getElementById('button-1').addEventListener('click', text1)
+document.getElementById('button-2').addEventListener('click', text2)
+document.getElementById('button-3').addEventListener('click', text3)
+document.getElementById('button-4').addEventListener('click', text4)
+document.getElementById('button-5').addEventListener('click', text5)
+
+/* jscpd:ignore-start */
+function text1 () {
+  document.getElementById('early-1').style.display = 'block'
+  document.getElementById('early-2').style.display = 'block'
+  document.getElementById('early-img').style.display = 'block'
+  document.getElementById('adult-1').style.display = 'none'
+  document.getElementById('adult-2').style.display = 'none'
+  document.getElementById('adult-img').style.display = 'none'
+  document.getElementById('adult-comment').style.display = 'none'
+  document.getElementById('later-1').style.display = 'none'
+  document.getElementById('later-2').style.display = 'none'
+  document.getElementById('later-img').style.display = 'none'
+  document.getElementById('impact').style.display = 'none'
+  document.getElementById('impact-1').style.display = 'none'
+  document.getElementById('links').style.display = 'none'
+  document.getElementById('text-1').style.display = 'none'
+}
+/* jscpd:ignore-end */
+
+/* jscpd:ignore-start */
+function text2 () {
+  document.getElementById('early-1').style.display = 'none'
+  document.getElementById('early-2').style.display = 'none'
+  document.getElementById('early-img').style.display = 'none'
+  document.getElementById('adult-1').style.display = 'block'
+  document.getElementById('adult-2').style.display = 'block'
+  document.getElementById('adult-img').style.display = 'block'
+  document.getElementById('adult-comment').style.display = 'block'
+  document.getElementById('later-1').style.display = 'none'
+  document.getElementById('later-2').style.display = 'none'
+  document.getElementById('later-img').style.display = 'none'
+  document.getElementById('impact').style.display = 'none'
+  document.getElementById('impact-1').style.display = 'none'
+  document.getElementById('links').style.display = 'none'
+  document.getElementById('text-1').style.display = 'none'
+}
+/* jscpd:ignore-end */
+
+/* jscpd:ignore-start */
+function text3 () {
+  document.getElementById('early-1').style.display = 'none'
+  document.getElementById('early-2').style.display = 'none'
+  document.getElementById('early-img').style.display = 'none'
+  document.getElementById('adult-1').style.display = 'none'
+  document.getElementById('adult-2').style.display = 'none'
+  document.getElementById('adult-img').style.display = 'none'
+  document.getElementById('adult-comment').style.display = 'none'
+  document.getElementById('later-1').style.display = 'block'
+  document.getElementById('later-2').style.display = 'block'
+  document.getElementById('later-img').style.display = 'block'
+  document.getElementById('impact').style.display = 'none'
+  document.getElementById('impact-1').style.display = 'none'
+  document.getElementById('links').style.display = 'none'
+  document.getElementById('text-1').style.display = 'none'
+}
+/* jscpd:ignore-end */
+
+/* jscpd:ignore-start */
+function text4 () {
+  document.getElementById('early-1').style.display = 'none'
+  document.getElementById('early-2').style.display = 'none'
+  document.getElementById('early-img').style.display = 'none'
+  document.getElementById('adult-1').style.display = 'none'
+  document.getElementById('adult-2').style.display = 'none'
+  document.getElementById('adult-img').style.display = 'none'
+  document.getElementById('adult-comment').style.display = 'none'
+  document.getElementById('later-1').style.display = 'none'
+  document.getElementById('later-2').style.display = 'none'
+  document.getElementById('later-img').style.display = 'none'
+  document.getElementById('impact').style.display = 'block'
+  document.getElementById('impact-1').style.display = 'block'
+  document.getElementById('links').style.display = 'none'
+  document.getElementById('text-1').style.display = 'none'
+}
+/* jscpd:ignore-end */
+
+/* jscpd:ignore-start */
+function text5 () {
+  document.getElementById('early-1').style.display = 'none'
+  document.getElementById('early-2').style.display = 'none'
+  document.getElementById('early-img').style.display = 'none'
+  document.getElementById('adult-1').style.display = 'none'
+  document.getElementById('adult-2').style.display = 'none'
+  document.getElementById('adult-img').style.display = 'none'
+  document.getElementById('adult-comment').style.display = 'none'
+  document.getElementById('later-1').style.display = 'none'
+  document.getElementById('later-2').style.display = 'none'
+  document.getElementById('later-img').style.display = 'none'
+  document.getElementById('impact').style.display = 'none'
+  document.getElementById('impact-1').style.display = 'none'
+  document.getElementById('links').style.display = 'block'
+  document.getElementById('text-1').style.display = 'none'
+}
+/* jscpd:ignore-end */
